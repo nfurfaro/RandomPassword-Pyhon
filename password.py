dcb476ea317be555455d60ecc035d7a23b1e40ca
Nick_Furfaro <nfurfaro33@gmail.com>
2016-04-18 10:04:57 -0600

    initial commit for my first python learning project.

---
+++ password.py
@@ -0,0 +1,14 @@
+#password generator
+
+import string
+import random
+
+sp = string.printable
+chars = list(string.strip(sp))
+length = int(input('How long a password would you like?:'))
+
+def main(l):
+  print string.join(map(str, (random.sample((chars),length))))
+
+if __name__ == '__main__':
+  main(length)
