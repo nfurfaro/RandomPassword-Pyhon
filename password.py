5c6a58e05ec3d28ace436a194597521f2aae95e1
Nick_Furfaro <nfurfaro33@gmail.com>
2016-04-19 12:33:54 -0600

    Adjusting indentation

--- password.py
+++ password.py
@@ -9,12 +9,12 @@ length = input('How long a password would you like?:')
 
 '''
 def hasNumbers(inputString):
-  return any(char.isdigit() for char in inputString)
+    return any(char.isdigit() for char in inputString)
 '''
 
 def main():
-  password = string.join(map(str, (random.sample((chars),length))))
-  print(password)
+    password = string.join(map(str, (random.sample((chars),length))))
+    print(password)
 
 if __name__ == '__main__':
-  main()
+    main()
